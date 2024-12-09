28f7b5ba7c31da517b0ef4f7c8174503d7bb7c40
zyqunix <fabiovanek2008@gmail.com>
2024-12-09 21:17:45 +0100

    i dunno

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ const { app, globalShortcut, shell, ipcMain, BrowserWindow, Tray, Menu, nativeIm
 const path = require('path')
 const fs = require('fs')
 const process = require('process')
-const {download} = require("electron-dl")
+const { download } = require("electron-dl")
 
 let mainWindow;
 
-function createWindow () {
-	// Create the browser window.
+function createWindow() {
 	mainWindow = new BrowserWindow({
 		width: 1920,
 		height: 1080,
@@ -16,24 +15,24 @@ function createWindow () {
 			nodeIntegration: true,
 			contextIsolation: false,
 			webviewTag: true,
-		}
+		},
+		maximized: true
 	})
 
 	mainWindow.setMenuBarVisibility(false)
 	mainWindow.loadFile('lib/index.html')
-
-	
 }
+
 
 // Root folders check
 if (!fs.existsSync("C://BetterSoundCloud")) {
-    fs.mkdirSync("C://BetterSoundCloud")
+	fs.mkdirSync("C://BetterSoundCloud")
 }
 if (!fs.existsSync("C://BetterSoundCloud/Downloads")) {
-    fs.mkdirSync("C://BetterSoundCloud/Downloads")
+	fs.mkdirSync("C://BetterSoundCloud/Downloads")
 }
 if (!fs.existsSync("C://BetterSoundCloud/assets")) {
-    fs.mkdirSync("C://BetterSoundCloud/assets")
+	fs.mkdirSync("C://BetterSoundCloud/assets")
 }
 
 
@@ -48,7 +47,7 @@ ipcMain.on("download", (event, info) => {
 
 
 function localPluginsAndThemes() {
-	
+
 }
 
 // This method will be called when Electron has finished
@@ -64,44 +63,44 @@ app.whenReady().then(() => {
 	})
 
 	// Prevent window from closing and quitting app
-    // Instead make close simply hide main window
-    // Clicking on tray icon will bring back main window
-    mainWindow.on('close', event => {
-        event.preventDefault()
-        mainWindow.hide()
-    })
+	// Instead make close simply hide main window
+	// Clicking on tray icon will bring back main window
+	mainWindow.on('close', event => {
+		event.preventDefault()
+		mainWindow.hide()
+	})
 
-    const icon = nativeImage.createFromPath('C:\\BetterSoundCloud\\assets\\bw-icon.ico')
-    let tray = new Tray(icon.resize({ width: 16, height: 16 }))
-    tray.setIgnoreDoubleClickEvents(true)
+	const icon = nativeImage.createFromPath('C:\\BetterSoundCloud\\assets\\bw-icon.ico')
+	let tray = new Tray(icon.resize({ width: 16, height: 16 }))
+	tray.setIgnoreDoubleClickEvents(true)
 
-    var trayMenu = Menu.buildFromTemplate([
-        {
-            label: 'Quit',
-            click: _ => {
-                console.log('Menu/Quit was clicked')
-                app.exit()
-            }
-        }
-    ]);
-    tray.setContextMenu(trayMenu)
+	var trayMenu = Menu.buildFromTemplate([
+		{
+			label: 'Quit',
+			click: _ => {
+				console.log('Menu/Quit was clicked')
+				app.exit()
+			}
+		}
+	]);
+	tray.setContextMenu(trayMenu)
 
-    // Prevent menu from being shown on left click
-    // Instead make main window visible (if it had been invisible)
-    tray.on('click', event => {
-        console.log('tray left clicked')
-        event.preventDefault
-        mainWindow.show()
-    })
+	// Prevent menu from being shown on left click
+	// Instead make main window visible (if it had been invisible)
+	tray.on('click', event => {
+		console.log('tray left clicked')
+		event.preventDefault
+		mainWindow.show()
+	})
 })
 
 // Handle Deep Links
 if (process.defaultApp) {
-    if (process.argv.length >= 2) {
-        app.setAsDefaultProtocolClient('bsc', process.execPath, [path.resolve(process.argv[1])])
-    }
+	if (process.argv.length >= 2) {
+		app.setAsDefaultProtocolClient('bsc', process.execPath, [path.resolve(process.argv[1])])
+	}
 } else {
-    app.setAsDefaultProtocolClient('bsc')
+	app.setAsDefaultProtocolClient('bsc')
 }
 
 const gotTheLock = app.requestSingleInstanceLock()
@@ -109,22 +108,22 @@ const gotTheLock = app.requestSingleInstanceLock()
 if (!gotTheLock) {
 	app.quit()
 } else {
-    app.on('second-instance', (event, commandLine) => {
-        // Someone tried to run a second instance, we should focus our window.
-        if (mainWindow) {
+	app.on('second-instance', (event, commandLine) => {
+		// Someone tried to run a second instance, we should focus our window.
+		if (mainWindow) {
 			if (mainWindow.isMinimized()) mainWindow.restore()
-            mainWindow.focus()
-        }
-        // the commandLine is array of strings in which last element is deep link url
-        // the url str ends with /
-        mainWindow.webContents.send("appDeepUrl", commandLine.pop())
-        console.log(commandLine)
-    })
+			mainWindow.focus()
+		}
+		// the commandLine is array of strings in which last element is deep link url
+		// the url str ends with /
+		mainWindow.webContents.send("appDeepUrl", commandLine.pop())
+		console.log(commandLine)
+	})
 
-    // Create mainWindow, load the rest of the app, etc...
-    // app.whenReady().then(() => {
-    //   createWindow()
-    // })
+	// Create mainWindow, load the rest of the app, etc...
+	// app.whenReady().then(() => {
+	//   createWindow()
+	// })
 }
 
 // Handle f5 and ctrl+r
@@ -138,11 +137,11 @@ app.on('browser-window-focus', function () {
 		mainWindow.webContents.send("appReqF5")
 		console.log("F5 is pressed");
 	});
-    globalShortcut.register("Esc", () => {
+	globalShortcut.register("Esc", () => {
 		mainWindow.webContents.send("appReqEsc")
 		console.log("Esc is pressed");
 	});
-    globalShortcut.register("MediaPlayPause", () => {
+	globalShortcut.register("MediaPlayPause", () => {
 		mainWindow.webContents.send("appReqMediaPlayPause")
 		console.log("MediaPlayPause is pressed");
 	});
@@ -162,37 +161,37 @@ app.on('browser-window-blur', function () {
 	globalShortcut.unregister('Esc');
 });
 
-ipcMain.on ("appReqClose", (event, args) => {
+ipcMain.on("appReqClose", (event, args) => {
 	app.quit()
 });
-ipcMain.on ("appReqMaximize", (event, args) => {
+ipcMain.on("appReqMaximize", (event, args) => {
 	if (mainWindow.isFullScreen()) {
 		mainWindow.setFullScreen(false)
 	} else {
 		mainWindow.setFullScreen(true)
 	}
 });
-ipcMain.on ("appReqJustFullscreen", (event, args) => {
+ipcMain.on("appReqJustFullscreen", (event, args) => {
 	mainWindow.setFullScreen(true)
 	mainWindow.webContents.send("appReqJustFullscreenUIFIX")
 });
-ipcMain.on ("appReqMinimize", (event, args) => {
+ipcMain.on("appReqMinimize", (event, args) => {
 	mainWindow.minimize()
 });
 // Restart window (made for handling custom cntrl + r and f5)
-ipcMain.on ("appReqRestartWindow", (event, args) => {
+ipcMain.on("appReqRestartWindow", (event, args) => {
 	mainWindow.reload()
 })
 
-ipcMain.on ("appReqClearCache", (event, args) => {
-    bscpath = process.env.APPDATA + "\\"+ "bettersoundcloud";
-	fs.rmSync(bscpath, {recursive: true, force: true });
-    mainWindow.reload()
+ipcMain.on("appReqClearCache", (event, args) => {
+	bscpath = process.env.APPDATA + "\\" + "bettersoundcloud";
+	fs.rmSync(bscpath, { recursive: true, force: true });
+	mainWindow.reload()
 })
 
 
 // Show downloaded item on file explorer
-ipcMain.on ("appDownloaderFinish", (event, args) => {
+ipcMain.on("appDownloaderFinish", (event, args) => {
 	shell.openPath('C:\\BetterSoundCloud\\Downloads')
 })
 // Quit when all windows are closed, except on macOS. There, it's common
